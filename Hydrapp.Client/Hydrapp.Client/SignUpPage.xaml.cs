cf86a33adc68e7dd25df31482be15ed479ed69ad
Ben Sterenson <bensterenson@gmail.com>
2017-01-11 19:03:25 +0200

    sign up works with azure

--- Hydrapp.Client/Hydrapp.Client/SignUpPage.xaml.cs
+++ Hydrapp.Client/Hydrapp.Client/SignUpPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hydrapp.Client.Modules;
+using Hydrapp.Client.Services;
+using System;
 using System.Linq;
 using Xamarin.Forms;
 
@@ -6,29 +8,35 @@ namespace Hydrapp.Client
 {
 	public partial class SignUpPage : ContentPage
 	{
-		public SignUpPage ()
+        private IService AzureDbService = App.AzureDbservice;
+        public SignUpPage ()
 		{
 			InitializeComponent();
 		}
 
 		async void OnSignUpButtonClicked (object sender, EventArgs e)
 		{
-			var user = new User () {
-				Username = usernameEntry.Text,
-				Password = passwordEntry.Text,
-				Email = emailEntry.Text,
-                Weight = weightEntry.Text,
-                Height = heightEntry.Text
-            };
 
+            var signUpSucceeded = validateInfo();
 			// Sign up logic goes here
 
-			var signUpSucceeded = AreDetailsValid (user);
 			if (signUpSucceeded)
             {
-				var rootPage = Navigation.NavigationStack.FirstOrDefault ();
+                var user = new User()
+                {
+                    userName = usernameEntry.Text,
+                    password = passwordEntry.Text,
+                    email = emailEntry.Text,
+                    weight = Double.Parse(weightEntry.Text),
+                    height = Double.Parse(heightEntry.Text)
+                };
+
+                var rootPage = Navigation.NavigationStack.FirstOrDefault ();
 				if (rootPage != null) {
 					App.IsUserLoggedIn = true;
+                    await AzureDbService.addUser(user);
+                    // user have UserId
+
                     //Navigation.InsertPageBefore (new MainPage(), Navigation.NavigationStack.First());
                     //await Navigation.PopToRootAsync ();
                     Navigation.InsertPageBefore(new MainPage(), this);
@@ -41,11 +49,30 @@ namespace Hydrapp.Client
 			}
 		}
 
-		bool AreDetailsValid (User user)
+		/*bool AreDetailsValid (User user)
 		{
             int n;
-			return (!string.IsNullOrWhiteSpace (user.Username) && !string.IsNullOrWhiteSpace (user.Password) && !string.IsNullOrWhiteSpace (user.Email) && user.Email.Contains ("@") &&
+			return (!string.IsNullOrWhiteSpace (user.userName) && !string.IsNullOrWhiteSpace (user.password) && !string.IsNullOrWhiteSpace (user.email) && user.email.Contains ("@") &&
                 !string.IsNullOrWhiteSpace(user.Weight) && int.TryParse(user.Weight, out n) && !string.IsNullOrWhiteSpace(user.Height) && int.TryParse(user.Weight, out n));
-		}
-	}
+		}*/
+        bool isNotNull(string value)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+
+        bool validateInfo()
+        {
+            double n;
+
+            if (double.TryParse(weightEntry.Text, out n) && double.TryParse(heightEntry.Text, out n))
+            {
+                if (isNotNull(usernameEntry.Text) && isNotNull(passwordEntry.Text) && isNotNull(emailEntry.Text))
+                {
+                    if (emailEntry.Text.Contains("@"))
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
 }
