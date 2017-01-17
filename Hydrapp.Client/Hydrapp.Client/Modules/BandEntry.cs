b6cf75f68a5a975699c13e2c7525b8cfc7a1d1dd
Noam Yosef <noamweinman@gmail.com>
2017-01-18 01:30:35 +0200

    create BandEntry

--- Hydrapp.Client/Hydrapp.Client/Modules/BandEntry.cs
+++ Hydrapp.Client/Hydrapp.Client/Modules/BandEntry.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hydrapp.Client.Helpers;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,33 @@ using System.Threading.Tasks;
 
 namespace Hydrapp.Client.Modules
 {
-    public class BandEntry
+    public class BandEntry :EntityData
     {
-        
+        [JsonProperty(PropertyName = "TimeStamp")]
+        public DateTime TimeStamp { get; set; }
+        [JsonProperty(PropertyName = "GroupId")]
+        public int GroupId { get; set; }
+        [JsonProperty(PropertyName = "UserId")]
+        public int UserId { get; set; }
+        [JsonProperty(PropertyName = "BandId")]
+        public int BandId { get; set; }
+        [JsonProperty(PropertyName = "GSR")]
+        public int GSR { get; set; }
+        [JsonProperty(PropertyName = "SkinTemp")]
+        public double SkinTemp { get; set; }
+        [JsonProperty(PropertyName = "Light")]
+        public int Light { get; set; }
+        [JsonProperty(PropertyName = "HeartRate")]
+        public int HeartRate { get; set; }
+        [JsonProperty(PropertyName = "UV")]
+        public int UV { get; set; }
+        [JsonProperty(PropertyName = "Calories")]
+        public int Calories { get; set; }
+        [JsonProperty(PropertyName = "Steps")]
+        public int Steps { get; set; }
+        [JsonProperty(PropertyName = "FluidLoss")]
+        public double FluidLoss { get; set; }
+        [JsonProperty(PropertyName = "IsDehydrated")]
+        public bool IsDehydrated { get; set; }
     }
 }
